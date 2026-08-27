262d62c4fc02e9eaf1b6612ff1f0fc361ad00bce
Ana Laura <ana.machioni@aluno.senai.br>
2026-08-27 09:01:08 -0300

    add: Ajustando css e o bootstrap

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,5 +1,6 @@
 import Square from "../Square/Square.jsx";
-import { calculateWinner } from "../../utils/calculateWinner.js";
+import styles from "./Board.module.css";
+import calculateWinner  from "../../utils/calculateWinner.js";
 
 
 export default function Board({ xIsNext, squares, onPlay }) {
@@ -26,18 +27,20 @@ export default function Board({ xIsNext, squares, onPlay }) {
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="board-row">
+      <div className={styles.status}>{status}</div>
+
+      <div className={styles.board}></div>
+      <div className={styles.boardRow}>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div className="board-row">
+      <div className={styles.boardRow}>
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div className="board-row">
+      <div className={styles.boardRow}>
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
